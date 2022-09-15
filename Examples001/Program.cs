b421d9db827bf91f84d765ae4e375d664e4bfc15
Alds <Aldsmsk@gmail.com>
2022-09-15 15:00:53 +0300

    Сложил два числа

---
+++ Examples001/Program.cs
@@ -0,0 +1,10 @@
+﻿// Console.Write:() в одну строку
+// Console.WriteLine:() В конце перейти на новую строку
+// Console.ReadLine:() Считать строку от пользователя
+
+Console.Write("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе числе: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+int sum = numberOne + numberTwo;
+Console.Write(sum);
