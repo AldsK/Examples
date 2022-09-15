0d5a1e6112affbee645e26205defd186356215a1
Alds <Aldsmsk@gmail.com>
2022-09-15 16:20:29 +0300

    Добавил сумму  чисел

---
+++ Exmaples02_sum/Program.cs
@@ -0,0 +1,9 @@
+﻿// Console.Write("Введите первое число: ");
+// int numberOne = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе числе: ");
+// int numberTwo = Convert.ToInt32(Console.ReadLine());
+// int sum = numberOne + numberTwo;
+// Console.Write(sum);
+// int numberA = 5;
+// int numberB = 6;
+// Console.WriteLine(numberA + numberB);
