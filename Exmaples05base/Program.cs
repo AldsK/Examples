b7bcf299bb05319489d6676bab92005352f4921e
Alds <Aldsmsk@gmail.com>
2022-09-16 18:33:39 +0300

    Нахождение числа

---
+++ Exmaples05base/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 8;
+int b = 2;
+int c = 6;
+int d = 1;
+int e = 4;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("Max: ");
+Console.WriteLine(max);
