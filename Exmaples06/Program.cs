ac1c751c438aed824ad687e786d41655e3a45303
Alds <Aldsmsk@gmail.com>
2022-09-17 18:14:45 +0300

    Рисунок

---
+++ Exmaples06/Program.cs
@@ -0,0 +1,22 @@
+﻿// Очищается консоль
+Console.Clear();
+// Отступ рисовать
+// Console.SetCursorPosition(10, 4);
+// Console.WriteLine("+");
+
+int xa = 5, ya = 1,
+    xb = 1, yb = 5,
+    xc = 10, yc = 5;
+int count = 0;
+while (count < 10)
+{
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+count += 1;
+}
